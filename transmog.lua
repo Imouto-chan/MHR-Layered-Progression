23f09d611c639f5d294b749332952a3e41d8a13a
Imouto-chan <76026763+Imouto-chan@users.noreply.github.com>
2022-01-28 23:53:36 -0500

    Researching hooking into methods, get player armor
    Figuring out how hooking into methods work and finding the player armor list for later usage. Interacting with c# lists through LUA is weird.

--- transmog.lua
+++ transmog.lua
@@ -7,15 +7,48 @@ local TransmogSet = {}
 
 local plOverwearIdList = sdk.get_managed_singleton("snow.data.EquipDataManager"):get_field("_PlOverwearMySetData"):get_field("_PlOverwearIdList")
 local dataTest = sdk.get_managed_singleton("snow.data.DataManager"):get_field("_PlOverwearBox")
+local dataArmorTest = sdk.get_managed_singleton("snow.data.DataManager"):get_field("_PlEquipBox")
+
+sdk.hook(sdk.find_type_definition("snow.data.EquipBox"):get_method("changeEquipment"), 
+function(args)
+	log.info("called")
+    --local armorId = sdk.to_managed_object(args[2])
+	--if armorId:get_field("value__") ~= nil then
+	--	log.info(tostring(armorId:get_field("value__")))
+	--end
+end,
+function(retval)
+	return retval;
+end
+)
 
 re.on_draw_ui(function()
 	imgui.text("Transmog:")
     if imgui.button("Transmog") then 
         --imgui.begin_child_window(100, true)
 		log.info("pushed")
-		TransmogSet[0] = plOverwearIdList[0]
+		--TransmogSet[0] = plOverwearIdList[0]
 		--sdk.get_managed_singleton("snow.data.EquipDataManager"):call("equipPlOverwear", TransmogSet[0]:get_field("Overwear_Head_105"))
-		dataTest:call("addPlOverwear", TransmogSet[0]:get_field("Overwear_Head_002"), false)
+		--dataTest:call("addPlOverwear", TransmogSet[0]:get_field("Overwear_Head_002"), false)
+		
+		--armorList = dataArmorTest:get_field("<EquipBoxArmorData>k__BackingField")
+		--if armorList ~= nil then
+		--	log.info("not nil")
+		--	log.info(tostring(armorList[0][0]:get_field("mSize")))
+		--end
+		armorList = dataArmorTest:get_field("<EquipBoxArmorData>k__BackingField")
+		if armorList ~= nil then
+			--log.info("not nil = " .. tostring(armorList:call("get_Item()", 0):call("get_Count()")))
+			log.info("not nil")
+			armorArray = armorList:call("get_Item(System.Int32)", 0)
+			if armorArray ~= nil then
+				log.info("not nil2 = " .. armorArray:call("get_Count()"))
+				item1 = armorArray:call("get_Item(System.Int32)", 0)
+				if item1 ~= nil then
+					log.info("not nil3 = " .. item1:get_field("mSize"))
+				end
+			end
+		end
     end
 	
 	if imgui.button("Unlock All Layered Armor") then
